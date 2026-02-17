fc87a8f95cf1b69bf639ed5520f5960f9cc4b290
Sachu013 <sachinsuresh.1311@gmail.com>
2026-02-17 10:56:28 +0530

    Code to display data

---
+++ display.java
@@ -0,0 +1,42 @@
+public class display
+{
+  private node head;
+
+  private static class node
+  {
+    private int data;
+    private node next;
+
+    public node(int data)
+  {
+    this.data =  data;
+    this.next =  null;
+  }
+
+  }
+
+  public void display()
+  {
+    node cur = head;
+    while(cur!=null)
+    {
+      System.out.print(cur.data +" -> ");
+      cur=cur.next;
+    }
+    System.out.println("null");
+  }
+
+  public static void main(String[] args) 
+  {
+    Insertmanually im = new Insertmanually();
+    im.head = new node(10);
+    node second = new node(20);
+    node third = new node(30);
+    
+    im.head.next = second;
+    second.next = third;
+
+    im.display();
+    
+  }
+}
