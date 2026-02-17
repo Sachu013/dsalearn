332f0f929ab3b360a1f21d89744dcea2a721fefd
Sachu013 <sachinsuresh.1311@gmail.com>
2026-02-17 10:07:16 +0530

    Single linked list Structure

---
+++ list.java
@@ -0,0 +1,18 @@
+public class list
+{
+  private ListNode head;
+
+  private static class ListNode
+  {
+    private int data;
+    private ListNode next;
+  }
+
+  public ListNode(int data)
+  {
+    this.data =  data;
+    this.next =  null;
+  }
+  
+  
+}
