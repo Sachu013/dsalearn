527b85cb17698a356aa4c68ae46ef3033eb7f40b
Sachu013 <sachinsuresh.1311@gmail.com>
2026-03-05 13:47:43 +0530

    wrote code to insert, traverse, search and validate a bst

--- binarysearch.java
+++ binarysearch.java
@@ -1,4 +1,4 @@
-import java.util.Stack;
+import java.util.*;
 
 public class binarysearch 
 {
@@ -23,7 +23,7 @@ public class binarysearch
       root = new node(val);
       return root;
     }
-    if(val<=root.data)
+    if(val<root.data)
     {
       root.left=insert(root.left,val);
     }
@@ -35,7 +35,21 @@ public class binarysearch
 
   }
 
-  
+  public node search(node root,int val)
+  {
+    if(root==null || root.data==val)
+    {
+      return root;
+    }
+    if(val<root.data)
+    {
+      return search(root.left,val);
+    }
+    else
+    {
+      return search(root.right,val);
+    }
+  }
 
   public void inorder(node root)
   {
@@ -61,7 +75,21 @@ public class binarysearch
     }
   }
 
+  public boolean valid(node root, int min, int max)
+  {
+    if(root==null)
+    {
+      return true;
+    }
+    if(root.data<=min || root.data>=max)
+    {
+      return false;
+    }
+    return valid(root.left,min,root.data) && valid(root.right,root.data,max);
+  }
+
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     binarysearch bs = new binarysearch();
     bs.root=bs.new node(25);
     bs.insert(bs.root,10);
@@ -70,6 +98,14 @@ public class binarysearch
     bs.insert(bs.root,40);
     bs.insert(bs.root,5);
     bs.inorder(bs.root);
+    if(bs.search(bs.root, sc.nextInt())!=null)
+    {
+      System.out.println("found");
+    }
+    else{
+      System.out.println("not found");
+    }
+    sc.close();
 
   }
   
