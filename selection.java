0a9a1e4aca31d594ab972f73b6d748da6da556d0
Sachu013 <sachinsuresh.1311@gmail.com>
2026-03-11 11:43:42 +0530

    wrote code for selection sort

---
+++ selection.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class selection 
+{
+  public static void selectionsort(int[] arr, int n)
+  {
+    for(int i=0;i<n-1;i++)
+    {
+      int min = i;
+      for(int j=i+1;j<n;j++)
+      {
+        if(arr[j]<arr[min])
+        {
+          min=j;
+        }
+      }
+      int temp = arr[min];
+      arr[min]=arr[i];
+      arr[i]=temp;
+
+    }
+  }
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n= sc.nextInt();
+    int[] arr = new int[n];
+    for(int i=0;i<n;i++)
+    {
+      arr[i]=sc.nextInt();
+    }
+       
+    selectionsort(arr,n);
+
+     for(int i=0;i<n;i++)
+    {
+      System.out.print(arr[i]+" ");
+    }
+    sc.close();
+  }
+  
+}
